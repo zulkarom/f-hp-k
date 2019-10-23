0d4500b32efe0538c98375c83c11c7be14047d88
zulkarom <zulkarom@gmail.com>
2019-10-23 10:41:50 +0800

    Update header.php

--- backend/views/layouts/header.php
+++ backend/views/layouts/header.php
@@ -8,7 +8,7 @@ use yii\helpers\Url;
 
 <header class="main-header">
 
-    <?= Html::a('<span class="logo-lg"><b>FKP</b> PORTAL</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+    <?= Html::a('<span class="logo-lg"><b>FHPK</b></span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
 
     <nav class="navbar navbar-static-top" role="navigation">
 
